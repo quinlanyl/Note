b4e1857148bf90b2efcd2f4aa85efdcf093e8580
Quinlan <xzky15@163.com>
2017-12-20 10:33:06 +0800

    dddd

--- colmap随笔.cpp
+++ colmap随笔.cpp
@@ -75,4 +75,93 @@ desc_->add_options()(name.c_str(),
             多次调用存在依次vector中 */
          ("include-path,I", value< vector<string> >(), 
              "include path")
- }						   
+ }	
+ #include "endian.h"
+ {
+	 reverse的用法
+	 {
+
+		 int main()
+		{
+			
+			std::vector<int> myvector;
+			for (int i = 1; i<10; ++i) myvector.push_back(i);   // 1 2 3 4 5 6 7 8 9  
+
+																//std::reverse(myvector.begin(), myvector.end());    // 9 8 7 6 5 4 3 2 1  
+			std::reverse(myvector.begin(), myvector.begin() + 5);    // 5 4 3 2 1 6 7 8 9  
+																	  //前五个反过来
+																	 // print out content:  
+			std::cout << "myvector contains:";
+			for (std::vector<int>::iterator it = myvector.begin(); it != myvector.end(); ++it)
+				std::cout << ' ' << *it;
+			std::cout << '\n';
+
+			std::string str{ "abcdef" };
+			std::reverse(str.begin(), str.end());
+			fprintf(stderr, "str: %s\n", str.c_str());
+			system("pause");
+			/*
+			40myvector contains: 5 4 3 2 1 6 7 8 9
+			str: fedcba
+	*/
+		}
+	 }
+	 // Check the order in which bytes are stored in computer memory.
+	bool IsLittleEndian();
+	bool IsBigEndian();
+	{
+	1． Little-endian：将低序字节存储在起始地址（低位编址）
+	2． Big-endian：将高序字节存储在起始地址（高位编址）
+	  一、大端和小端的问题
+
+	对于整型、长整型等数据类型，Big endian 认为第一个字节是最高位字节（按照从低地址到高地址的顺序存放数据的高位字节到低位字节）；而 Little endian 则相反，它认为第一个字节是最低位字节（按照从低地址到高地址的顺序存放据的低位字节到高位字节）。
+
+	例如，假设从内存地址 0x0000 开始有以下数据：  
+	0x0000         0x0001       0x0002       0x0003  
+	0x12            0x34           0xab           0xcd 
+	如果我们去读取一个地址为 0x0000 的四个字节变量，若字节序为big-endian，则读出结果为0x1234abcd；若字节序为little-endian，则读出结果为0xcdab3412。
+
+	如果我们将0x1234abcd 写入到以 0x0000 开始的内存中，则Little endian 和 Big endian 模式的存放结果如下：  
+	地址           0x0000         0x0001        0x0002          0x0003 
+	big-endian   0x12           0x34            0xab            0xcd  
+	little-endian  0xcd           0xab            0x34            0x12
+
+	一般来说，x86 系列 CPU 都是 little-endian 的字节序，PowerPC 通常是 big-endian，网络字节顺序也是 big-endian还有的CPU 能通过跳线来设置 CPU 工作于 Little endian 还是 Big endian 模式。
+
+	对于0x12345678的存储：
+
+	小端模式：（从低字节到高字节）
+	地位地址 0x78 0x56 0x34 0x12 高位地址
+
+	大端模式：（从高字节到低字节）
+	地位地址 0x12 0x34 0x56 0x78 高位地址
+	}
+	T LittleEndianToNative(const T x);//先判断那种类型，再反转
+	template <typename T>
+	T BigEndianToNative(const T x);
+	template <typename T>
+	T NativeToLittleEndian(const T x);
+	template <typename T>
+	T NativeToBigEndian(const T x);
+	T ReadBinaryLittleEndian(std::istream* stream) 
+	{
+	  T data_little_endian;
+	  //将stream中的内容写到data_little_endian ，只写sizeof（T）个字节
+	  stream->read(reinterpret_cast<char*>(&data_little_endian), sizeof(T));
+	  return LittleEndianToNative(data_little_endian);
+	} 
+	template <typename T>
+	void WriteBinaryLittleEndian(std::ostream* stream, const T& data) 
+	{
+	  const T data_little_endian = NativeToLittleEndian(data);
+	  //与上述相反，
+	  stream->write(reinterpret_cast<const char*>(&data_little_endian), sizeof(T));
+	}
+	//重载
+	void WriteBinaryLittleEndian(std::ostream* stream, const std::vector<T>& data)
+	 {//容器遍历
+	  for (const auto& elem : data) {
+		WriteBinaryLittleEndian<T>(stream, elem);
+	  }
+	}
+ }
